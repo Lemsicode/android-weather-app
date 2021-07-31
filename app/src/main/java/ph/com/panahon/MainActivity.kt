6227819547db6d8b910d89061dc90b970c2401c5
Lemsic <lemsicode@gmail.com>
2021-07-31 15:15:35 +0800

    Beta Test 1.0

--- app/src/main/java/ph/com/panahon/MainActivity.kt
+++ app/src/main/java/ph/com/panahon/MainActivity.kt
@@ -8,4 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+//    sana nakikita niyo to po, hello po. HAHAHAAH
 }
