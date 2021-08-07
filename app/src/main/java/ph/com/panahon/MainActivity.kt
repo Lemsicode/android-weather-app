25dd7e05825639fb80438e6377f9af6704f90cff
Lemsic <lemsicode@gmail.com>
2021-08-07 22:54:56 +0800

    Beta Test 2.0 - Added Launcher Icon, Navigation Bar, Fragments, Color Palettes, and Icons. Switching of Fragments are now functional.

--- app/src/main/java/ph/com/panahon/MainActivity.kt
+++ app/src/main/java/ph/com/panahon/MainActivity.kt
@@ -2,19 +2,48 @@ package ph.com.panahon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ph.com.panahon.fragments.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(ForecastFragment())    //Load the Forecast Fragment as Default Fragment
+
+        // Find the BottomNavView, and add listeners for respective fragments
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView?.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_forecast -> {
+                    loadFragment(ForecastFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_search -> {
+                    loadFragment(SearchFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_settings -> {
+                    loadFragment(SettingsFragment())
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    loadFragment(ForecastFragment())
+                    return@setOnItemSelectedListener true
+                }
+            }
+        }
     }
 
-
-//    sana nakikita niyo to po, hello po. HAHAHAAH
-    //SECOND TEST!!!!!!!!!
-    // THIRD TEST HELLO GUYS
-    // FOURTH TEST PO ETO.
-    // FIFTH TEST NA PO ETO.
-    // SIXTH POCHING TEST PO ETO :)
-    // LAST NA ITO. SEVENTH TEST.
+    /**
+     * This method loads the fragment being passed into the parameter.
+     */
+    private fun loadFragment(fragment : Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_fragment_holder, fragment)
+        fragmentTransaction.commit()
+    }
 }
